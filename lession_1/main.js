16b75d5de4958a2760aa209a2f15bf4f59933dff
VaanDat <datphamvan1510@gmail.com>
2022-09-20 23:54:53 +0700

    Add lab1

--- lession_1/main.js
+++ lession_1/main.js
@@ -1,1 +1,14 @@
-alert ('This is a new page');
+var myObject = {
+    name: 'Van Dat',
+    age: 18,
+    address: 'TP HCM',
+};
+console.log(myObject);
+
+var myArray =[
+    'Javascript',
+    'PHP',
+    'Ruby',
+
+];
+console.log(myArray);
